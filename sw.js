f90cc0287788a15e43a530c18d17fde6014c2690
39yh <39yukikoharu@gmail.com>
2026-08-17 03:37:54 +0900

    feat: シェア用のOGP画像とメタタグを追加

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'myshelf-v4';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'myshelf-v5';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './og.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
